7fc5c5ac9d8ef1ee96fbac364f02d8cff6914258
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2026-08-01 18:26:09 -0500

    chore: fix

--- introduction-to-zig/1-introducing-zig/hello_world/src/hello_world.zig
+++ introduction-to-zig/1-introducing-zig/hello_world/src/hello_world.zig
@@ -2,10 +2,5 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    const multiline =
-        \\hello
-        \\\bar
-    ;
     try stdout.print("Hello, {s}!\n", .{"world"});
-    try stdout.print(multiline);
 }
